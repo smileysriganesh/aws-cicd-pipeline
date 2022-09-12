e9420d2bb787ae56740f3595fc8cef58b4f84c8f
smileysriganesh <sriganeshgsg1996@gmail.com>
2022-09-12 16:16:24 +0530

    state file provider details upgrade

--- state.tf
+++ state.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQF2MZLLE3IXDGXMR"
-  secret_key = "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"
+  access_key = "AKIAQF2MZLLEZQNLFYNW"
+  secret_key = "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"
 }
